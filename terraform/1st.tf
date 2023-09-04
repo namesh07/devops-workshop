dd9ca2d62b1c817f93a04292be3f6cfe1062acf9
Namesh Banale <namesh@Nameshs-MacBook-Air.local>
2023-09-04 23:46:30 +0530

    added tf with vpc

--- terraform/1st.tf
+++ terraform/1st.tf
@@ -46,3 +46,76 @@ tags = {
 }
 
 }
+
+
+//create vpc
+
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpp-vpc"
+
+  }
+  
+}
+
+//create subnet
+
+resource "aws_subnet" "dpp-public_subent_01" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpp-public_subnet_01"
+
+    }
+
+}
+
+resource "aws_subnet" "dpp-public_subent_02" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+    tags = {
+      Name = "dpp-public_subnet_02"
+
+    }
+
+}
+
+//creating internet gateway˳
+
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  tags = {
+    Name = "dpp-igw"
+  }
+  
+}
+
+//create route table
+
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id
+  }  
+  tags = {
+    Name = "dpp-public-rt"
+  }
+}
+
+//association subnet with route table
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+  subnet_id = aws_subnet.dpp-public_subent_01.id
+  route_table_id = aws_route_table.dpp-public-rt.id  
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+  subnet_id = aws_subnet.dpp-public_subent_02.id
+  route_table_id = aws_route_table.dpp-public-rt.id  
+}
